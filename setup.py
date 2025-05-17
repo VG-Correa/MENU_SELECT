2b7c4a0b9763844c7fdd32817b6af1d839f42723
VG-Correa <v.victorgabriel2014@gmail.com>
2025-05-17 09:31:48 -0300

    Finalizado imports

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="menu_select",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(),
     install_requires=[
         # Liste suas dependências aqui
